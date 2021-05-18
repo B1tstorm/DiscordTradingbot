866ec2e4a8c4478660b9fe068c31f8d8e0482b11
Neytex <fokodiamond@gmail.com>
2021-05-18 23:00:01 +0200

    Klassen zusammengeführt und ans Observerpattern angepasst

--- src/main/java/de/fh_kiel/discordtradingbot/Interaction/ChannelInteracter.java
+++ src/main/java/de/fh_kiel/discordtradingbot/Interaction/ChannelInteracter.java
@@ -1,35 +1,35 @@
 package de.fh_kiel.discordtradingbot.Interaction;
 
-public class ChannelInteracter implements EventListener {
-	private String tag;
-	private String channelCommand;
+public class ChannelInteracter {
+    public EventManager events;
+    private String channelCommand;
 
-	public void operation() {
-		// TODO - implement ChannelInteracter.operation
-		throw new UnsupportedOperationException();
-	}
+    public void operation() {
+        // TODO - implement ChannelInteracter.operation
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * 
-	 * @param message
-	 */
-	public Boolean writeMessage(String message) {
-		// TODO - implement ChannelInteracter.writeMessage
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * @param message
+     */
+    public Boolean writeMessage(String message) {
+        // TODO - implement ChannelInteracter.writeMessage
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 *
-	 * @param emoji
-	 * @return Boolean
-	 */
-	public Boolean reactEmoji(String emoji) {
-		// TODO - implement ChannelInteracter.reactEmoji
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * @param emoji
+     * @return Boolean
+     */
+    public Boolean reactEmoji(String emoji) {
+        // TODO - implement ChannelInteracter.reactEmoji
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void update(String eventType, Integer price, Integer eventId, String product) {
-		System.out.println("EventType: " + eventType + " Preis: " + price);
-	}
+    // TODO Channel abhören
+    public void listenToChannel(String eventType, Integer price, Integer eventId, String product) {
+        if (channelCommand.equals("!bid")) {
+            events.notify(eventType, price, eventId, product);
+        }
+    }
 }
