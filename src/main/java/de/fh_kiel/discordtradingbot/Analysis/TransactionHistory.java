7ffc21bb77c41e7e0ced9ef76f9eeb6dfa598e7a
Jonas Hermann Becker <jonas.h.becker@student.fh-kiel.de>
2021-06-03 13:19:13 +0200

    Value als Integer Änderung eingearbeitet

--- src/main/java/de/fh_kiel/discordtradingbot/Analysis/TransactionHistory.java
+++ src/main/java/de/fh_kiel/discordtradingbot/Analysis/TransactionHistory.java
@@ -38,13 +38,13 @@ public class TransactionHistory implements EventListener, Publisher {
     }
 
     private Letter extractLetterObject(EventItem item) {
-        return new Letter(item.getProduct()[0], 1, Integer.parseInt(item.getValue()));
+        return new Letter(item.getProduct()[0], 1, item.getValue());
     }
 
     // Builder Pattern
     private TransactionHistoryItem createTransactionHistoryItem(EventItem eventItem) {
         return new TransactionHistoryItemBuilder().setTransactionId(Integer.parseInt(eventItem.getAuctionId()))
-                .setValue(Integer.parseInt(eventItem.getValue()))
+                .setValue(eventItem.getValue())
                 .setSellerId(eventItem.getSellerID())
                 .setTraderId(eventItem.getTraderID())
                 .setProduct(eventItem.getProduct())
